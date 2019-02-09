e18a7075087aa2769b6cdbbc85db06cccdb69f53
Raiymbek <40425204+MrAtkens@users.noreply.github.com>
2019-02-09 23:49:09 +0600

    Add files via upload

---
+++ Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+
+	[SerializeField]
+ private Text scoreText;
+
+ 	[SerializeField]
+ private Player player;
+
+ private float score=0;
+
+ private void Update() {
+	score =  player.transform.position.z;
+	scoreText.text ="Счёт: " + score.ToString("0");
+ }
+
+}
---
+++ Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+	[SerializeField]
+	private Rigidbody rigidBody;
+	
+	[SerializeField]
+	private float speed;
+
+	[SerializeField]
+	private float sideSpeed;
+
+	private float spawnX= -0.473f;
+	private float spawnY=	-0.703f;
+	private float spawnZ=	-5.34f;
+
+	void Start () {
+		
+	}
+	void Update () {
+		if(Input.GetKey(KeyCode.A)){
+			//Move left
+		rigidBody.AddForce(-sideSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			//Move right
+		rigidBody.AddForce(sideSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		else if(Input.GetKey(KeyCode.W))
+		{
+			//Move front
+		rigidBody.AddForce(0, 0, speed * Time.deltaTime, ForceMode.VelocityChange);
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+		rigidBody.AddForce(0, 0, -speed * Time.deltaTime, ForceMode.VelocityChange);
+		}
+	}
+
+	private void OnCollisionEnter(Collision other) {
+	
+	if(other.gameObject.tag == "Obstacle"){
+		
+		rigidBody.AddForce(0,0,-speed*100);
+
+	}	
+
+	}
+
+	private void OnTriggerEnter(Collider other) {
+
+
+		if(other.gameObject.tag=="Relocation"){
+
+		  this.transform.position= new Vector3(spawnX, spawnY, spawnZ);
+
+		}
+
+	  if(other.tag == "Springboard"){
+			rigidBody.AddForce(0,500,0);	
+		}
+
+		if(other.gameObject.tag=="Coin"){
+			//destroy object
+			Destroy(other.gameObject);
+
+			speed=speed+5;
+			sideSpeed=sideSpeed+2;
+
+			Debug.Log("+1 COIN");
+		}
+				
+		if(other.gameObject.tag=="Finish"){
+			
+			this.enabled=false;
+
+			Debug.Log("FOU WON");
+		}
+	}
+
+
+}
